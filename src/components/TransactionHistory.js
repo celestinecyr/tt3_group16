fd9da3515ad382d3ac46e5cb40284d4fb0f602c3
Camellia Lok <camellialok.2017@sis.smu.edu.sg>
2021-03-05 14:50:06 +0800

    Displaying the tables for buy and sell order type

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -7,32 +7,69 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const TransactionHistory = ({ data, setData }) => {
     const test = [{ 'id': 1 }, { 'id': 2 }]
     return (
-        <div>
-            <table class="table">
-                <tr>
-                    <th>Transaction ID</th>
-                    <th>Order Type</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Asset Symbol</th>
-                    <th>Asset Price</th>
-                    <th>Asset Amount</th>
-                    <th>Cash Amount</th>
-                </tr>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">Buy</div>
+                        <div class="card-body">
+                            <table class="table table-sm">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>Transaction ID</th>
+                                        <th>Date & Time</th>
+                                        <th>Asset Symbol</th>
+                                        <th>Asset Price</th>
+                                        <th>Asset Amount</th>
+                                        <th>Cash Amount</th>
+                                    </tr>
+                                </thead>
 
-                {data.map((d) => (
-                    <tr key={d.transactionId}>
-                        <td>{d.transactionId}</td>
-                        <td>{d.orderType}</td>
-                        <td>{moment.unix(d.timestamp).format('MMMM do YYYY')}</td>
-                        <td>{moment.unix(d.timestamp).format('h:mm:ss a')}</td>
-                        <td>{d.assetSymbol}</td>
-                        <td>{d.assetPrice}</td>
-                        <td>{d.assetAmount}</td>
-                        <td>{d.cashAmount}</td>
-                    </tr>
-                ))}
-            </table>
+                                {data.map((d) => (d.orderType == 'BUY' ?
+                                    <tr key={d.transactionId}>
+                                        <td>{d.transactionId}</td>
+                                        <td>{moment.unix(d.timestamp).format('d/M/yy HH:mm')}</td>
+                                        <td>{d.assetSymbol}</td>
+                                        <td>{d.assetPrice}</td>
+                                        <td>{d.assetAmount}</td>
+                                        <td>{d.cashAmount}</td>
+                                    </tr> : null
+                                ))}
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">Sell</div>
+                        <div class="card-body">
+                            <table class="table table-sm">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>Transaction ID</th>
+                                        <th>Date & Time</th>
+                                        <th>Asset Symbol</th>
+                                        <th>Asset Price</th>
+                                        <th>Asset Amount</th>
+                                        <th>Cash Amount</th>
+                                    </tr>
+                                </thead>
+
+                                {data.map((d) => (d.orderType == 'SELL' ?
+                                    <tr key={d.transactionId}>
+                                        <td>{d.transactionId}</td>
+                                        <td>{moment.unix(d.timestamp).format('d/M/yy HH:mm')}</td>
+                                        <td>{d.assetSymbol}</td>
+                                        <td>{d.assetPrice}</td>
+                                        <td>{d.assetAmount}</td>
+                                        <td>{d.cashAmount}</td>
+                                    </tr> : null
+                                ))}
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
