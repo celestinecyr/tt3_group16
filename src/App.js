c5cd02053d20616cc7bbebccd35272aef0eab3a8
NgJianWei <ngjianwei456@gmail.com>
2021-03-05 16:10:33 +0800

    final commit before merge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 //import { PriceData } from './components/PriceData';
 import axios from 'axios';
 import  { useState, useEffect } from 'react'
+import { render } from '@testing-library/react';
 
 function App() {
 
@@ -10,8 +11,22 @@ function App() {
   const[timestamp, setTimestamp] = useState('');
 
   function loadTransactionData() {
-    
+    let priceAPI = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current'
+        axios.defaults.headers.common = {
+            "X-API-Key": "LAmt7e4YU21vFGBwHT6s4aOdBR040NqE1WUd7XKD",
+        };
+        axios.post(priceAPI, {}).then(function (response) {
+            setPrice(JSON.stringify(response.data.price));
+            setAssetSymbol(JSON.stringify(response.data.assetSymbol));
+            setTimestamp(JSON.stringify(response.data.timestamp));
+            console.log(response.data);
+            //return response.data; //return this Object instead of printing
+
+        }).catch(function (error) {
+            console.log(error);
+        });
   }
+
   useEffect(() => {
     async function loadTransactionData() {
         let priceAPI = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current'
@@ -31,14 +46,17 @@ function App() {
     }
     loadTransactionData()
 }, [])
-  return (
-    <div className="App">
-          <button onClick="window.location.reload();">Get updated price</button>
-          <h3>Asset Symbol : {assetSymbol}</h3>
-          <h3>Asset Price : {price}</h3>
-          <h3>Timestamp : {timestamp}</h3>
-    </div>
-  );
+
+  render(); {
+    return (
+      <div className="App">
+            <button onClick="history.go(0)">Get updated price</button>
+            <h3>Asset Symbol : {assetSymbol}</h3>
+            <h3>Asset Price : {price}</h3>
+            <h3>Timestamp : {timestamp}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
