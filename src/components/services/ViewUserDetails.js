2d5c65c47b569be648c82c379ee153a04c90709a
celestinecyr <celestinecyr@gmail.com>
2021-03-05 13:28:25 +0800

    update

--- src/components/services/ViewUserDetails.js
+++ src/components/services/ViewUserDetails.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import DashHeader from './dashboardheader/DashboardHeader';
+import Axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
-export class ViewUserDetails extends Component {
-    render() {
+function ViewUserDetails() {
+
+    const user = localStorage.getItem("token");
+    const decoded = jwt_decode(user);
+    console.log(decoded);
+
+    // useEffect(() => {
+    //     fetchUserDetails()
+    // })
+
+    // const fetchUserDetails() => {
+    //    Axios.post("http://localhost:3000/login")
+    //    {
+    //        id:decoded.id
+    //    }).then((response)=>{
+    //        if(respose) {
+    //            console.log(response)
+    //        }
+    //    })
+    // }
         return (
             <div>
-                
+                <DashHeader />
+
             </div>
         )
-    }
 }
 
-export default ViewUserDetails
+export default ViewUserDetails;
--- src/components/services/dashboardheader/DashboardHeader.js
+++ src/components/services/dashboardheader/DashboardHeader.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Link, useHistory } from 'react-router-dom';
-import dbslogo from '../../images/dbslogo.svg';
+import dbslogo from '../../../images/dbslogo.svg';
 import './dashheader.styles.css'
 import { Jumbotron } from 'reactstrap';
 import jwt_decode from 'jwt-decode';
@@ -18,9 +18,9 @@ export default function DashHeader() {
         window.location.reload();
     }
 
-    const user = localStorage.getItem("token");
-    const decoded = jwt_decode(user);
-    console.log(decoded.id.usernameLogin);
+    // const user = localStorage.getItem("token");
+    // const decoded = jwt_decode(user);
+    // console.log(decoded);
 
         return (
             <>
@@ -56,7 +56,7 @@ export default function DashHeader() {
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
                                 <div>
-                                    <h1>Welcome {decoded.id.username}, </h1>
+                                    <h1>Welcome, </h1>
                                     <h1>DBS iBanking</h1>
                                     <p>can insert stuff here</p>
                                 </div>
