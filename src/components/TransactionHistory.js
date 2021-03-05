25a7025247a0755985d220b7335b02a04067eadd
Camellia Lok <camellialok.2017@sis.smu.edu.sg>
2021-03-05 13:44:55 +0800

    Creating a table with transaction details

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -2,14 +2,35 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import moment from 'moment';
 
-export const TransactionHistory = ({data, setData}) => {
-    const test = [{'id':1},{'id':2}]
+export const TransactionHistory = ({ data, setData }) => {
+    const test = [{ 'id': 1 }, { 'id': 2 }]
     return (
         <div>
-            {data.map((d) => (
-                <p key={d.transactionId}>{d.transactionId}</p>
-            ))}
+            <table>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Order Type</th>
+                    <th>Date</th>
+                    <th>Time</th> 
+                    <th>Asset Symbol</th>
+                    <th>Asset Price</th>
+                    <th>Asset Amount</th>
+                    <th>Cash Amount</th>
+                </tr>
 
+                {data.map((d) => (
+                    <tr key={d.transactionId}>
+                        <td>{d.transactionId}</td>
+                        <td>{d.orderType}</td>
+                        <td>{moment.unix(d.timestamp).format('MMMM do YYYY')}</td>
+                        <td>{moment.unix(d.timestamp).format('h:mm:ss a')}</td>
+                        <td>{d.assetSymbol}</td>
+                        <td>{d.assetPrice}</td>
+                        <td>{d.assetAmount}</td>
+                        <td>{d.cashAmount}</td>
+                    </tr>
+                ))}
+            </table>
         </div>
 
 
