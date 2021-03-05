0d10ce099b70acf0ebca92257ba93a91224e0967
NgJianWei <ngjianwei456@gmail.com>
2021-03-05 15:24:13 +0800

    Show results to webpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 //import { PriceData } from './components/PriceData';
 import axios from 'axios';
-import moment from 'moment';
 import  { useState, useEffect } from 'react'
 
 function App() {
-  
-  const [data, setData] = useState([])
-  let assetSymbol = null;
-  let price = null;
-  let timestamp = null;
 
+  const[price, setPrice] = useState('');
+  const[assetSymbol, setAssetSymbol] = useState('');
+  const[timestamp, setTimestamp] = useState('');
+
+  function loadTransactionData() {
+    
+  }
   useEffect(() => {
     async function loadTransactionData() {
         let priceAPI = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current'
@@ -18,14 +19,10 @@ function App() {
             "X-API-Key": "LAmt7e4YU21vFGBwHT6s4aOdBR040NqE1WUd7XKD",
         };
         axios.post(priceAPI, {}).then(function (response) {
-            setData(response.data)
-            //console.log(moment.unix(response.data[0]['timestamp']).format('dddd MMMM Do YYYY, h:mm:ss a'));
-            assetSymbol = response.data.assetSymbol;
-            price = response.data.price;
-            timestamp = response.data.timestamp;
-            console.log("assetSymbol is " + assetSymbol + " price is " + price + " timestamp is " + timestamp)
+            setPrice(JSON.stringify(response.data.price));
+            setAssetSymbol(JSON.stringify(response.data.assetSymbol));
+            setTimestamp(JSON.stringify(response.data.timestamp));
             console.log(response.data);
-
             //return response.data; //return this Object instead of printing
 
         }).catch(function (error) {
@@ -36,7 +33,10 @@ function App() {
 }, [])
   return (
     <div className="App">
-          <button onClick=''>Get Asset Price.</button>
+          <button onClick="window.location.reload();">Get updated price</button>
+          <h3>Asset Symbol : {assetSymbol}</h3>
+          <h3>Asset Price : {price}</h3>
+          <h3>Timestamp : {timestamp}</h3>
     </div>
   );
 }
