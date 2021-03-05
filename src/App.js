d51deb699da4b4f44f067f3073642c0f2645f8b4
nigel-02 <46718879+nigel-02@users.noreply.github.com>
2021-03-05 15:46:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -118,12 +118,5 @@ function App() {
 }
 
 
-function InfoField(){
-  return (
-    <div className="input-group mb-3">
-    <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-    </div>
-  )
-}
 
 export default App;
