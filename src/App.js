9ce2032a2b4ef586c29c1ead36250da72d9ea508
Camellia Lok <camellialok.2017@sis.smu.edu.sg>
2021-03-05 13:11:00 +0800

    Calling of API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { TransactionHistory } from './components/TransactionHistory';
+import axios from 'axios';
+import moment from 'moment';
+import  { useState, useEffect } from 'react'
 
 function App() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    async function loadTransactionData() {
+        let transactionAPI = 'https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view'
+        axios.defaults.headers.common = {
+            "X-API-Key": "LAmt7e4YU21vFGBwHT6s4aOdBR040NqE1WUd7XKD",
+        };
+        axios.post(transactionAPI, {
+            "accountKey": "4cb6dbea-a84c-4b29-ad43-2c69182681ab"
+        }).then(function (response) {
+            setData(JSON.parse(response))
+            console.log(moment.unix(response.data[0]['timestamp']).format('dddd MMMM Do YYYY, h:mm:ss a'));
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+    loadTransactionData()
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <TransactionHistory data={data} setData={setData}>
+        
+      </TransactionHistory>
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +41,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import moment from 'moment';
+
+export const TransactionHistory = ({data, setData}) => {
+    const test = [{'id':1},{'id':2}]
+    return (
+        // <div>
+        //     {/* data:{JSON.stringify(data)} */}
+        //     {/* {data} */}
+        //     {JSON.stringify(test)}
+        //     <p key=
+        //     {/* {this.test.map((t) => {t.id})} */}
+        //     {/* {this.loadTransactionData()} */}
+        //     {/* {JSON.stringify({"key":"1010"})} */}
+        //     {/* {} */}
+        //     {/* {data} */}
+        //     test
+        // </div>
+
+
+    )
+}
